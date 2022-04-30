3216467fe89e814b358dae3b985868c3b30730a9
xyily <56762822+xyily@users.noreply.github.com>
2022-04-30 22:40:58 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,5 +5,7 @@ import numpy as np
 st.title('X-Ray Classification Team - A Brief Introduction')
 st.write("Hello, we are Maxi and Ilayda.")
 st.balloons()
-st.image('chest-pneumoia.jpeg', caption="hi", width=None, channels="RGB", output_format="auto")
+
 result=st.button("Click here")
+if result:
+  st.image('chest-pneumoia.jpeg', caption="hi", width=None, channels="RGB", output_format="auto")
