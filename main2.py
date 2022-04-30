5fa2e9e15274c18970b4a725681c69fe1f2c8fe1
xyily <56762822+xyily@users.noreply.github.com>
2022-04-30 22:47:10 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -6,11 +6,11 @@ st.title('X-Ray Classification Team - A Brief Introduction')
 st.write("Hello, we are Maxi and Ilayda.")
 st.balloons()
 
-result-normal=st.button("Click here to see the x-ray image of a normal chest")
+resultnormal=st.button("Click here to see the x-ray image of a normal chest")
 if resultnormal:
   st.image('Normal-chest.jpeg', caption="this is what a normal chest looks like", width=None, channels="RGB", output_format="auto")
   
-result-pneumonia=st.button("Click here to see the x-ray image of a chest with pneumonia")
+resultpneumonia=st.button("Click here to see the x-ray image of a chest with pneumonia")
 if resultpneumonia:
   st.image('chest-pneumoia.jpeg', caption="this is what a chest with pneumonia looks like", width=None, channels="RGB", output_format="auto")
  
