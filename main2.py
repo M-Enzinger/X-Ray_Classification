b9c2a0734683ede861defef73e48ae0543de4d7c
xyily <56762822+xyily@users.noreply.github.com>
2022-04-30 22:31:17 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,4 +5,4 @@ import numpy as np
 st.title('X-Ray Classification Team - A Brief Introduction')
 st.write("Hello, we are Maxi and Ilayda.")
 st.balloons()
-st.image("https://github.com/M-Enzinger/X-Ray_Classification/blob/main/chest-pneumoia.jpeg", caption="hi", width=None, channels="RGB", output_format="auto")
+st.image('chest-pneumoia.jpeg', caption="hi", width=None, channels="RGB", output_format="auto")
