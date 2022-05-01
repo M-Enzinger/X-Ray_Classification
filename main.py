ee625c0aab71e78f9cb353897bc8bc752eb249aa
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 13:12:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 st.title('X-Ray Classification Team - A Brief Introduction')
 st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
 st.write("In the picture below you can see the X-RAY of a chest. You recognize something unusual? Neither we.")
-st.write("Inspect the picture carefully, then click the Button below.")
+st.write("Inspect the picture carefully, then click the Button.")
 
 
 col1, col2, col3 , col4, col5 = st.columns(5)
