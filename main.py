699e9721b5f87e62797a1275e44cf89c39b8220d
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 12:34:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 import numpy as np
 
 st.title('X-Ray Classification Team - A Brief Introduction')
-st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
-st.write("Test")
+st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation."/n)
+st.write("In the picture below you can see the X-RAY of a chest. You recognize something unusual? Neither we.")
+st.write("Inspect the picture carefully, then click the Button below")
 
 
 
