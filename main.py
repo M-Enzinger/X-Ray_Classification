487a10dd1aa00fb4d31ab9209cebb5baa5c8f963
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 12:36:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title('X-Ray Classification Team - A Brief Introduction')
 st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
 st.write("In the picture below you can see the X-RAY of a chest. You recognize something unusual? Neither we.")
-st.write("Inspect the picture carefully, then click the Button below")
+st.write("Inspect the picture carefully, then click the Button below.")
 
 
 
