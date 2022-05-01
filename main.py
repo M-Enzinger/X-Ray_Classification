5fa8828734889babdc6b6f83c525c0f546dea27d
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 12:30:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import pandas as pd
 import numpy as np
 
 st.title('X-Ray Classification Team - A Brief Introduction')
-st.write("Hello")
+st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
+st.write("Test")
+
+
+
+
+
 st.balloons()
