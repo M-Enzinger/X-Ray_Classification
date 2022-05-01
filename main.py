5701a48f8700badf9cd11ed0b8d38abec39eb93a
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 13:39:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,12 @@ with col5:
 
 #Second part - solution and motivation - firstly unvisible
 if button1:
+    #Solution what user has missed
     st.image(image="chest-pneumoia_prepared.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.write("Probably you have not detected it, but this person has a dangerous pneumonia, highlighted on the appeared picture above.")
     st.write("You can compare both, a healthy lung and one with pneumonia by clicking these radio buttons:")
     
-    
+    #Raadio Button to switch between a healthy lung abnd one with pneumonia
     col1, col2, col3 , col4, col5 = st.columns(5)
 
     with col1:
@@ -45,11 +46,19 @@ if button1:
 
 
     if radio_button1 == 'a healthy lung':
-        button1 = true
         st.image(image="Normal-chest.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-        button1 = true
     else:
         st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+        
+    #Motivation
+    st.write("As demonstrated, it is not possible to identify a pneumonia as a non medical. Even physicians sometimes fail in recognising dangerrous lung deseases.")
+    st.write("Therefore we want to create a machine learned based model as a posible solution. This model will capable of identifying pneumonia on the bases of X-RAYs.")
+    
+    chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
 
 else:
     st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
