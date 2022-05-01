367caceb3e0c38cf3615a91c2588ed03116d68a1
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 14:56:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import numpy as np
 import requests
 import json
 
-def healthy_lung():
-     st.image(image="Normal-chest.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-     
-def pneumonia_lung():
-      st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-          
+
 def first_part():
      #First part - introduction and question - visible
      st.title('X-Ray Classification Team - A Brief Introduction')
@@ -33,10 +28,10 @@ def second_part():
         ('a healthy lung', 'a lung with pneumonia'))
 
     if radio_button1 == 'a healthy lung':
-        healthy_lung()
+          st.image(image="Normal-chest.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
      
     else:
-        pneumonia_lung()
+          st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
       
         
      
