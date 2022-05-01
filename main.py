89c5f2e37ad28626da875c19c585170e0b3a666e
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 13:11:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+#First part - introduction and question - visible
 st.title('X-Ray Classification Team - A Brief Introduction')
 st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
 st.write("In the picture below you can see the X-RAY of a chest. You recognize something unusual? Neither we.")
@@ -16,15 +17,25 @@ with col1:
 with col2:
     pass
 with col3:
-    button1 = st.button("What do I have missed?")
+    button1 = st.button("What have I missed?")
 with col4:
     pass
 with col5:
     pass
 
+#Second part - solution and motivation - firstly unvisible
 if button1:
-  st.image(image="chest-pneumoia_prepared.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image(image="chest-pneumoia_prepared.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.write("Probably you have not detected it, but this person has a dangerous pneumonia, highlighted on the second picture above. You can compare both, a helty lung and one with pneumonia by clicking these radio buttons:")
+    
+    radio_button1 = st.radio(
+     "I want to see the X-RAY of...",
+     ('a healthy lung', 'a lung with pneumonia'))
 
+    if radio_button1 == 'a healthy lung':
+        st.image(image="Normal-chest.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    if radio_button1 == 'a lung with pneumonia':
+        st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
   
 
 
