520653f795ef177d1fe1e0eb6f4c3f59f5dab5fb
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 13:27:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ if button1:
      st.write("I want to see the X-RAY of...")
     with col3:
         radio_button1 = st.radio(
-        "",
+        ,
         ('a healthy lung', 'a lung with pneumonia'))
     with col4:
        pass
