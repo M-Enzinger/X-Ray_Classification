b4c48e1ae13da663e8934d8863e239aaffc52458
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-01 13:12:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ st.title('X-Ray Classification Team - A Brief Introduction')
 st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
 st.write("In the picture below you can see the X-RAY of a chest. You recognize something unusual? Neither we.")
 st.write("Inspect the picture carefully, then click the Button below.")
-st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
 
 col1, col2, col3 , col4, col5 = st.columns(5)
@@ -36,7 +35,10 @@ if button1:
         st.image(image="Normal-chest.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     if radio_button1 == 'a lung with pneumonia':
         st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-  
+
+else:
+    st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
 
 
 
