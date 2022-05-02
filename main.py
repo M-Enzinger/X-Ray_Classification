8714abf48667a2a08ef61d56a163df269aa7da0c
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-02 13:03:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,8 @@ import numpy as np
 import requests
 import json
 
-
-def first_part():
-     #First part - introduction and question - visible
-     st.title('X-Ray Classification Team - A Brief Introduction')
-     st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
-     st.write("In the picture below you can see the X-RAY of a chest. You recognize something unusual? Neither we.")
-     st.write("Inspect the picture carefully, then click the Button.")
-     st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")   
-     
-     col1, col2, col3 , col4, col5 = st.columns(5)
-     with col3:
-          button1 = st.button("What have I missed?")   
-        
-     if button1:
-          second_part()
-
-     #Weather forecast
+def weather():
+          #Weather forecast
      st.write("To make sure that you are dressed approriate to avoid a pneumonia, you can check today`s Weather by choosing your city below:")
 
      #User chooses location
@@ -31,15 +16,12 @@ def first_part():
      if loc == 'Erlangen':
           lat = "49.599941"
           lon = "11.006300"
-          button1 = True
      elif loc == 'Nuremberg':
           lat = "49.452103"
           lon = "11.076665"
-          button1 = True
      elif loc == 'Forchheim':
           lat = "49.719910"
           lon = "11.058220"
-          button1 = True
      
      #Get Data from API
      api_key = "41c76f28ad89e9493b1aa62dac513ba2"
@@ -57,6 +39,31 @@ def first_part():
      col1.metric("Temperature", str(tempr) + " °C")
      col2.metric("Wind", str(wind) + " m/s")
      col3.metric("Humidity", str(hum) + " %")
+     
+     
+def first_part():
+     #First part - introduction and question - visible
+     st.title('X-Ray Classification Team - A Brief Introduction')
+     st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
+     st.write("In the picture below you can see the X-RAY of a chest. You recognize something unusual? Neither we.")
+     st.write("Inspect the picture carefully, then click the Button.")
+     st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")   
+     
+     col1, col2, col3 , col4, col5 = st.columns(5)
+     with col3:
+          button1 = st.button("What have I missed?")   
+          
+     col1, col2, col3 , col4, col5 = st.columns(5)
+     with col3:
+          button1 = st.button("What have I missed?")   
+        
+     if button1:
+          second_part()
+          
+     if button1:
+          weather()
+
+
           
      
      
