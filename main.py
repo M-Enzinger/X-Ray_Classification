223302b7410c8edc80d4fe7922694aa54d6b29eb
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-02 13:36:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 def first_part():
      #First part - introduction and question - visible
      st.title('X-Ray Classification Team - A Brief Introduction')
-     st.write("Hello, we are Ilayda and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
+     st.write("Hello, we are Ilayda, Jan and Maximilian, the X-Ray Classifiaction team. Subsequently we will introduce our project and motivation.")
      st.write("In the picture below you can see the X-RAY of a chest. You recognize something unusual? Neither we.")
      st.write("Inspect the picture carefully, then click the Button.")
      st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")   
