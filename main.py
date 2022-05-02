a405c6076b55ea1ed7ae792494c531734304c8fa
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-05-02 13:31:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,18 @@ def first_part():
      st.write("Inspect the picture carefully, then click the Button.")
      st.image(image="chest-pneumoia.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")   
      
+     #Button1 to show answer, motivation, chart
      col1, col2, col3 , col4, col5 = st.columns(5)
      with col3:
           button1 = st.button("What have I missed?")   
         
      if button1:
+          #Executes second part to show answer, motivation, chart
           second_part()
 
-     #Weather forecast
+          
+     #Weather:
+     #Weather forecast text
      st.write("To make sure that you are dressed approriate to avoid a pneumonia, you can check today`s Weather by choosing your city below:")
 
      #User chooses location
@@ -59,20 +63,19 @@ def first_part():
      col3.metric("Humidity", str(hum) + " %")
           
      
-     
 def second_part():  
-    #Second part - solution and motivation - firstly unvisible
+    #Second part - solution and motivation - firstly unvisible - triggered by Button1
     #Solution what user has missed
     st.image(image="chest-pneumoia_prepared.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.write("Probably you have not detected it, but this person has a dangerous pneumonia, highlighted on the appeared picture above.")
     st.write("You can compare both, a healthy lung and one with pneumonia by clicking these radio buttons:") 
         
-     
     #Motivation
     st.write("As demonstrated, it is not possible to identify a pneumonia as a non medical. Even physicians sometimes fail in recognising dangerous lung deseases.")
     st.write("Therefore we want to create a machine learned based model as a possible solution. This model will be capable of identifying pneumonia on the bases of X-RAYs.")
-    st.write("To state how important the recognition of lung deseases is, we built a chart of all deaths caused by pneumonia per 100th citizens per year in specific EU countries:")
+    st.write("To state how important the recognition of lung deseases is, we provide a chart of all deaths caused by pneumonia per 100th citizens per year in specific EU countries:")
     
+    #Chart
     st.image("pneumonia_chart.PNG", caption="Pneumonia deaths in Germany between 1998 and 2019", width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     
      
@@ -80,10 +83,4 @@ def second_part():
 first_part()
 
 
-
-    
-             
-
-
-#ideen: wetter standort dropdown menü; 
 #st.balloons()
