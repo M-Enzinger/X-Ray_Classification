4b23f3db86ca5f412c52a057a76f040ac8222e5a
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-06-16 16:31:20 +0200

    testztsdvcwe

--- main.py
+++ main.py
@@ -109,9 +109,9 @@ with st.expander("Chapter 1: Business Understanding"):
 with st.expander("Chapter 2: Data Preparation"):
     st.title("Chapter 2: Data Preparation")
     st.write("At first we reviewed our Data set and checked the relative balances between test, training and validation data. The given data set contained:")
-    st.info("training set: 5,216 files [89%] belonging to two classes, 1,341 to 'NORMAL' [26%] and 3,875 to 'PNEUMONIA' [74%]")
-    st.info("test set: 624 files [11%] belonging to two classes, 234 to 'NORMAL' [36%] and 390 to 'PNEUMONIA' [64%]")
-    st.info("validation set: 16 [<1%] files belonging to two classes, 8 to 'NORMAL' [50%] and 8 to 'PNEUMONIA' [50%]")
+    st.warning("training set: 5,216 files [89%] belonging to two classes, 1,341 to 'NORMAL' [26%] and 3,875 to 'PNEUMONIA' [74%]")
+    st.warning("test set: 624 files [11%] belonging to two classes, 234 to 'NORMAL' [36%] and 390 to 'PNEUMONIA' [64%]")
+    st.warning("validation set: 16 [<1%] files belonging to two classes, 8 to 'NORMAL' [50%] and 8 to 'PNEUMONIA' [50%]")
     st.write("As you can see we have some unbalances we have to deal with: First of all we moved around 5% of the training data to the test data. Because of the fact that we have a relatively small dataset in sum, we considered a 84%/16% ratio as appropriate.")
     st.write("Additionally we created some augmented picture of the class 'NORMAL' in the training set to get a 40%/60% ratio of both classes. We used vertical flip and up to 20% zoom range for the augmentation (we defined the output size as 1200x1200 even if we use a smaller one later, but downsizing is always easier than upsizing. Of course we will use augmentation in the model as well, but we would not solve the problem of a dominant class this way.")
     st.write("We didn´t touched the validation data set yet.")
@@ -158,6 +158,7 @@ with st.expander("Chapter 3: First CN-Network"):
              caption="2. Data Augmentation Graphic Card Monitor (Cuda Kernels)",
              width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 with st.expander("Chapter 4: Validation, Visualisation And Bias Verification"):
+    st.title("Chapter 4: Validation, Visualisation And Bias Verification")
     st.subheader("Validation - Understanding The Metrics")
     st.write("Before measuring values like the accuracy of sensitivity we have to understand them - a brief summary:")
     st.image("chapter4_accsensspec.PNG",
@@ -170,7 +171,7 @@ with st.expander("Chapter 4: Validation, Visualisation And Bias Verification"):
     st.write("At first we only measured the Accuracy. But we recognized that the validation accuracy can be very misleading. An example: If your model has a specificity of nearly 100% and a sensitivity of 40%, the accuracy could still be around 80%, even if the model only detects 40% of all PNEUMONIA lungs as not healthy.")
     st.write("We implemented the sensitivity and specificity the following way:")
     st.image("chapter4_accsensspecimpl.PNG",
-             caption="Accuracy, Sensitivity, Specificity implementation, Page 1",
+             caption="Accuracy, Sensitivity, Specificity implementation",
              width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.subheader("Validation - Plotting the Metrics")
     st.write("To get a better overview we plotted the accuracy, validation accuracy, specificity, validation specificity, sensitivity, validation sensitivity and loss as well as validation loss")
@@ -180,10 +181,37 @@ with st.expander("Chapter 4: Validation, Visualisation And Bias Verification"):
     st.write("BILDER DER VISUALISIERUNG CODE ERKLÄRUNG ETC!!!")
 
 with st.expander("Chapter 5: Performance, System and CO2 Emission"):
-    st.write(" ")
+    st.title("Chapter 5: Performance, System and CO2 Emission")
+    st.subheader("Operating System")
+    st.info("CPU: AMD RYZON 7 1700x; Threads: 16; Cores: 8; CPU Clock: 3.8 mhz")
+    st.info("GPU: GeForce GTX 1080; Cores: 2560; Memory Size: 8GB; Memory Type: GDDR5X")
+    st.info("RAM: 15GB; DDR4")
+    st.subheader("Performance and Resources Monitoring")
+    st.write("All Screenshots were captured while training our latest model")
+    st.image("chapter5_ram.PNG",
+             caption="RAM Usage",
+             width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("chapter5_gpu.PNG",
+             caption="GPU And GPU Memory Usage; Most Important: Cuda Load",
+             width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("chapter5_temp.PNG",
+             caption="CPU And Mainboard Temperature; Cooling System: Water-cooling",
+             width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.success("Training Time (per 10 epochs): 32 min.")
+    st.success("Time per Step: 400-650 ms/step.")
+    st.subheader("CO2 Emission")
+    st.write("To measure the amount of CO2 emission due to training a model, we first measured the consumption of the PC")
+    st.warning("Energy Consumption: < 450W")
+    st.write("Taking into account the time for training and the german energy mix...")
+    st.error("CO2 Emission Per kWh (german energy mix): 420g CO2")
+    st.error("CO2 Emission Per training (10epochs): 420g*(32/60)*0.45 = 100,8g CO2")
+    st.error("CO2 Emission Per Model Development (50-100x training 10epochs each): 5.040kg-10.080kg CO2")
+    st.warning("This Amount equals a 80 to 100 km car ride")
 with st.expander("Chapter 6: Fine Tuning"):
-    st.write(" ")
+    st.title("Chapter 6: Fine Tuning")
 with st.expander("Chapter 7: Second CN-Network: Can A Pre-Trained Model Outperform Our Own CNN?"):
-    st.write(" ")
+    st.title("Chapter 7: Second CN-Network: Can A Pre-Trained Model Outperform Our Own CNN?")
 with st.expander("Chapter 8: Interactive Online Test of both CNNs"):
-    st.write(" ")
+    st.title("Chapter 8: Interactive Online Test of both CNNs")
+with st.expander("Chapter 9: Conclusion"):
+    st.title("Chapter 9: Conclusion")
