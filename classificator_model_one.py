4dbf8527abd0f35c6d1b84e69f4480daea1730e6
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-06-21 00:55:31 +0200

    Update classificator_model_one.py

--- classificator_model_one.py
+++ classificator_model_one.py
@@ -4,7 +4,8 @@ from tensorflow import keras
 import streamlit as st
 
 uploaded_img = st.file_uploader("Upload Your X-Ray Here")
-st.button('Click Here To Classify'):
+button = st.button("Click Here To Classify")
+if button:
     test_image = image.load_img('uploaded_img', target_size = (600, 600))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis = 0)
