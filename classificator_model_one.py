4b51e7bb2e3a366b4950330baa14998442920b1a
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-06-20 23:27:00 +0200

    Update classificator_model_one.py

--- classificator_model_one.py
+++ classificator_model_one.py
@@ -3,7 +3,7 @@ from tensorflow.keras.preprocessing import image
 from tensorflow import keras
 import streamlit as st
 
-uploaded_img = st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
+uploaded_img = st.file_uploader('Upload Your X-Ray Here, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
 test_image = image.load_img('uploaded_img', target_size = (600, 600))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
