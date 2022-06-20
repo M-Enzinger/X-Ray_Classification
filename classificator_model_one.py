1caeeb445dad631efa973318bd4c91f0b78b4b1f
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-06-20 23:21:58 +0200

    Update classificator_model_one.py

--- classificator_model_one.py
+++ classificator_model_one.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 import streamlit as st
 
 uploaded_img = st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
-test_image = image.load_img('uploaded_img', target_size = (512, 512))
+test_image = image.load_img('uploaded_img', target_size = (600, 600))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 model = keras.models.load_model('https://www.webdesign-hosting.eu/ml4b/saved_xray_classifier.h5')
