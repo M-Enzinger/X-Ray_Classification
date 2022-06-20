0c1ee36b4efd13cbfadb18007a008c55da59db47
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-06-21 00:58:49 +0200

    Update classificator_model_one.py

--- classificator_model_one.py
+++ classificator_model_one.py
@@ -6,7 +6,7 @@ import streamlit as st
 uploaded_img = st.file_uploader("Upload Your X-Ray Here")
 button = st.button("Click Here To Classify")
 if button:
-    test_image = image.load_img('uploaded_img', target_size = (600, 600))
+    test_image = image.load_img(uploaded_img, target_size = (600, 600))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis = 0)
     model = keras.models.load_model('https://www.webdesign-hosting.eu/ml4b/saved_xray_classifier.h5')
