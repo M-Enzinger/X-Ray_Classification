6fcdac39a24da781ae819447a9c0c25e6e70f78d
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-06-23 10:32:38 +0200

    Update Image-Abfrage.py

--- Image-Abfrage.py
+++ Image-Abfrage.py
@@ -11,82 +11,99 @@ from torchvision import datasets, models, transforms
 import os
 import torch.nn as nn
 
-os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+image_file = st.file_uploader("Choose a file to predict and classify")
+if image_file is not None:
+    file_details = {"FileName":image_file.name, "FileType":image_file.type}
+    st.write(file_details)
+    img = load_image(image_file)
+    st.image(img, height=600, widht=600)
+    
+    with open(os.path.join("tempDir", image_file.name, "wb") as f:
+              f.write(image_file.getbuffer())
+col1, col2, col3, col4, col5 = st.columns(5)
+        with col3:
+            button1 = st.button("Predict")
+
+if (startButton):
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-# In[2]:
+    # In[2]:
 
 
-# Data augmentation and normalization for training
-# Just normalization for validation
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'test': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
 
-# Directory of 'train' and 'test' folders
-data_dir = "C:/Users/Jan/Desktop/XRAY/chest_xray/chest_xrayEqualImages/"
+    uploaded_file = st.file_uploader("Choose a file to predict and classify")
 
-# Retrieving the images by folder
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
+    # Directory of 'train' and 'test' folders
+    data_dir = iploaded_file
+
+    # Retrieving the images by folder
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                              data_transforms[x])
+                      for x in ['train', 'test']}
+
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                 shuffle=True, num_workers=4)
                   for x in ['train', 'test']}
-
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                             shuffle=True, num_workers=4)
-              for x in ['train', 'test']}
-class_names = image_datasets['train'].classes
+    class_names = image_datasets['train'].classes
 
 
-# In[3]:
+    # In[3]:
 
 
-model = models.resnet18()
-num_ftrs = model.fc.in_features
+    model = models.resnet18()
+    num_ftrs = model.fc.in_features
 
-# Here the size of each output sample is set to 2.
-model.fc = nn.Linear(num_ftrs, 2)
+    # Here the size of each output sample is set to 2.
+    model.fc = nn.Linear(num_ftrs, 2)
 
-model.load_state_dict(torch.load("C:/Users/Jan/Desktop/XRAY/RetrainedResNetModel.pt"))
+    model.load_state_dict(torch.load("/models/FULLRetrainedResNetModel.pt"))
 
-model = model.to(device)
-
-
-# In[4]:
+    model = model.to(device)
 
 
-number_of_predictions = 1
-
-with torch.no_grad():
-    for data in dataloaders['test']:
-        images, labels = data
-        
-        plt.imshow(np.squeeze(images[0][0:1]), cmap='gray')
-        
-        images = images.to(device)
-        labels = labels.to(device)
-        
-        pred_outputs = model(images)
-        predicted = torch.argmax(pred_outputs.data, 1)
-
-print('True Value: ', ' '.join('%s' % class_names[predicted[j]] for j in range(number_of_predictions)))
-print('Predicted: ', ' '.join('%s' % class_names[predicted[0]]
-                              for j in range(number_of_predictions)))
+    # In[4]:
 
 
-# In[ ]:
+    number_of_predictions = 1
 
+    with torch.no_grad():
+        for data in dataloaders['test']:
+            images, labels = data
+
+            plt.imshow(np.squeeze(images[0][0:1]), cmap='gray')
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            pred_outputs = model(images)
+            predicted = torch.argmax(pred_outputs.data, 1)
+
+    print('True Value: ', ' '.join('%s' % class_names[predicted[j]] for j in range(number_of_predictions)))
+    print('Predicted: ', ' '.join('%s' % class_names[predicted[0]]
+                                  for j in range(number_of_predictions)))
+
+
+    # In[ ]:
+
+    
 
 
 
