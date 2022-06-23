679087d7cfe746486d346272a94f651e11a2366f
JanEhrFAU <93187468+JanEhrFAU@users.noreply.github.com>
2022-06-23 18:22:08 +0200

    Update main3.py

--- main3.py
+++ main3.py
@@ -11,13 +11,13 @@ with st.expander("Chapter 1: Business Understanding"):
     #von ilayda kopieren
 with st.expander("Chapter 2: Data Preparation"):
     st.title("Chapter 2: Data Preparation")
-    st.write("At first we reviewed our Data set and checked the relative balances between test, training and validation data. The given data set contained:")
+    st.write("At first we reviewed our data set and checked the relative balances between test, training and validation data. The given data set contained:")
     st.warning("training set: 5,216 files [89%] belonging to two classes, 1,341 to 'NORMAL' [26%] and 3,875 to 'PNEUMONIA' [74%]")
     st.warning("test set: 624 files [11%] belonging to two classes, 234 to 'NORMAL' [36%] and 390 to 'PNEUMONIA' [64%]")
     st.warning("validation set: 16 [<1%] files belonging to two classes, 8 to 'NORMAL' [50%] and 8 to 'PNEUMONIA' [50%]")
-    st.write("As you can see we have some unbalances we have to deal with: First of all we moved around 5% of the training data to the test data. Because of the fact that we have a relatively small dataset in sum, we considered a 84%/16% ratio as appropriate.")
-    st.write("Additionally we created some augmented picture of the class 'NORMAL' in the training set to get a 40%/60% ratio of both classes. We used vertical flip and up to 20% zoom range for the augmentation (we defined the output size as 1200x1200 even if we use a smaller one later, but downsizing is always easier than upsizing. Of course we will use augmentation in the model as well, but we would not solve the problem of a dominant class this way.")
-    st.write("We didn´t touched the validation data set yet.")
+    st.write("As you can see we had some uneven distribution we had to deal with: First of all we moved around 5% of the training data to the test data. Because of the fact that we have a relatively small dataset in sum, we considered a 84%/16% ratio as appropriate.")
+    st.write("Additionally we created some augmented pictures of the class 'NORMAL' in the training set to get a 40%/60% ratio of both classes. We used vertical flip and up to 20% zoom range for the augmentation (we defined the output size as 1200x1200 even if we use a smaller one later, but downsizing is always easier than upsizing. Of course we will use augmentation in the model as well, but we would not solve the problem of a dominant class this way.")
+    st.write("We hadn´t touched the validation data set yet.")
     st.write("At the end, our data set looked like this:")
     st.success("training set: 6154 files [84%] belonging to two classes, 2400 to 'NORMAL' [39%] and 3,754 to 'PNEUMONIA' [61%]")
     st.success("test set: 1158 files [16%] belonging to two classes, 486 to 'NORMAL' [42%] and 672 to 'PNEUMONIA' [58%]")
@@ -26,11 +26,11 @@ with st.expander("Chapter 2: Data Preparation"):
     st.image("img/chapter2_code1.png",
              caption="Data Generator Code",
              width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-    st.write("We also recognized some potential bias sources, like the sensors on many PNEUMONIA pictures. But we decided to check on potential biases later by visualizing a trained model and finding potential solutions, if needed, later. To deal with the different picture sizes, we standardize them in Chapter 3 under 'Data Generator And Augmentation' ")
+    st.write("We also recognized some potential bias sources, like the sensors on many PNEUMONIA pictures. But we decided to check on potential biases later by visualizing a trained model and finding potential solutions, if needed, later. To deal with the different picture sizes, we standardized them in Chapter 3 under 'Data Generator And Augmentation' ")
 with st.expander("Chapter 3: First CN-Network Including Augmentation"):
     st.title("Chapter 3: First CN-Network Including Augmentation")
     st.subheader("CNN Architecture")
-    st.write("As out third step we build our first CNN using Tensorflow and Keras. On the following pictures you can study the code and settings we used at the end, after testing and validating dozens of variations (regarding amount of layers, augmentation settings, filter sizes, padding, pooling size, batch sizes and much more).")
+    st.write("As our third step we built our first CNN using Tensorflow and Keras. On the following pictures you can study the code and settings we used at the end, after testing and validating dozens of variations (regarding amount of layers, augmentation settings, filter sizes, padding, pooling size, batch sizes and much more).")
     st.image("img/chapter3_arch1.png",
              caption="CNN Architecture",
              width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
@@ -53,14 +53,14 @@ with st.expander("Chapter 3: First CN-Network Including Augmentation"):
     st.info("Steps per epoch: whole dataset")
     st.info("Optimizer: adam")
     st.subheader("Data Generator And Augmentation")
-    st.write("At first we used the tensorflow Data Generator, we have had to realize that the Generator causes big performance losses.")
+    st.write("At first, we used the tensorflow Data Generator, but unfortunately had to learn that the Generator causes big performance losses.")
     st.image("img/chapter3_firstdatagen.png",
              caption="1. Data Generator",
              width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.image("img/chapter3_firstdatagengraf.png",
-             caption="1. Data Generator Graphic Card Monitor (Cuda Kernels)",
+             caption="1. Data Generator Graphics Card Monitor (Cuda Kernels)",
              width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-    st.write("As a better alternative we found the following way to implement data augmentation (which was very importent to us because of our relatively small amount of data).")
+    st.write("As a better alternative we found the following way to implement data augmentation (which was very important to us because of our relatively small amount of data).")
     st.image("img/chapter3_seconddatagen.png",
              caption="2. Data Augmentation",
              width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
@@ -79,7 +79,7 @@ with st.expander("Chapter 4: Validation, Visualization And Bias Verification Of 
     st.info("Specificity: The specificity shows the performance of detecting a NORMAL lung if someone is really healthy. In Other words: The specificity contribute to the rate of false Positive/ true negativ detections. In reference to the Picture above: TN/(TN + FP)")
     st.info("Accuracy: The accuracy shows the overall and combined performance of the model trough all classes. In reference to the Picture above:  (TN + TP)/(TN+TP+FN+FP)")
     st.subheader("Validation - Measuring")
-    st.write("At first we only measured the Accuracy. But we recognized that the validation accuracy can be very misleading. An example: If your model has a specificity of nearly 100% and a sensitivity of 40%, the accuracy could still be around 80%, even if the model only detects 40% of all PNEUMONIA lungs as not healthy.")
+    st.write("At first we only measured the accuracy. But we eventually found out that the validation accuracy can be very misleading. An example: If your model has a specificity of nearly 100% and a sensitivity of 40%, the accuracy could still be around 80%, even if the model only detects 40% of all PNEUMONIA lungs as not healthy.")
     st.write("We implemented the sensitivity and specificity the following way, including an automated stop with the best weights (training until 30th epoch and recover best weights:")
     st.image("img/chapter4_accsensspecimpl.png",
              caption="Accuracy, Sensitivity, Specificity implementation",
@@ -96,10 +96,10 @@ with st.expander("Chapter 4: Validation, Visualization And Bias Verification Of 
              caption="Early Stopping",
              width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.subheader("Validation - Visualization!")
-    st.write("To visualize our classification we first tried to use a function provided by the xception pretrained model, after that we created and trained a completely new pytorch model to visualize our classification but in the end we managed it to visualize it in our original Keras model,")
-    st.write("Here should be pictures of our visualization, but as you can see, you can see nothing")
+    st.write("To visualize our classifications we first tried to use a function provided by the xception pretrained model, after that we created and trained a completely new pytorch model to visualize our classification but hoped that, in the end, we would manage to visualize it in our original Keras model.")
+    st.write("Here the pictures of our visualization should be, but as you can see, you can see nothing")
     st.write("That´s because we had some struggle implementing visualization into a Keras model, so we followed the advice to build a pytorch model and to visualize this one.")
-    st.write("That´s why you will find the last two chapters (3 and 4) repeating in chapter 5 and 6, just with our new pytorch model...")
+    st.write("For that reason, you will find the last two chapters (3 and 4) repeating in chapter 5 and 6, just with our new pytorch model...")
     st.subheader("Our Overall Performance Of The First CNN")
     st.success("Validation Accuracy: 87%")
     st.success("Validation Sensitivity: 83%")
