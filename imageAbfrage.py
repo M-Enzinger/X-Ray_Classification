962cb6448300266a54ddee7de6bf5d66f115214d
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2022-06-23 22:45:52 +0200

    Update imageAbfrage.py

--- imageAbfrage.py
+++ imageAbfrage.py
@@ -19,7 +19,7 @@ def load_image(image_file):
     img = Image.open(image_file)
     return img
 
-image_file = st.file_uploader("Upload An Image")
+image_file = st.file_uploader("Upload An Image To Classify")
 if image_file is not None:
     file_details = {"FileName":image_file.name,"FileType":image_file.type}
     st.write(file_details)
